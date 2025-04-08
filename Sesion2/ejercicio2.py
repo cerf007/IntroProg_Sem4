1fc7148785c230f7cf1853ba904c3aeac46a0d99
cerf007 <cerf02062007@gmail.com>
2025-04-08 11:00:32 -0600

    Ejercicios de la sesión 2

--- Sesion2/ejercicio2.py
+++ Sesion2/ejercicio2.py
@@ -1,2 +1,10 @@
 #Declara tres variables numéricas y evalúa la siguiente expresión: (a + b) * c / 2.
 #Muestra el resultado y explica cómo se evalúa la expresión paso a paso
+print("Bienvenido a tu calculadora de la expresión (a + b) * c / 2")
+variable_a = float(input("Ingrese el valor númerico a: "))
+variable_b = float(input("Ingrese el valor númerico b: "))
+variable_c = float(input("Ingrese el valor númerico c: "))
+operacion = float((variable_a + variable_b) * variable_c) / 2
+print("Primero se realiza la suma de varibale a", variable_a, "con la variable b", variable_b)
+print("Luego, del resultado del parenteris se multiplica con la varible c", variable_c)
+print("Por último, se divide el todo eel resulatado entre 2 para obtener", operacion)
