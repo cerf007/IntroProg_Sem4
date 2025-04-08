18357fedd581c00cafb49b6c53a8ffcd7a94c38d
cerf007 <cerf02062007@gmail.com>
2025-04-08 11:28:33 -0600

    Ejercicio de la sesión 2

---
+++ Sesion2/ejercicio4.py
@@ -0,0 +1,9 @@
+#Declara una variable de tipo texto que contenga un número (por ejemplo, "25").
+#Luego convierte ese valor a número e imprímelo incrementado en 1.
+texto = "25"
+print(texto)
+print(type(texto))
+
+numero = float(texto) + 1
+print(numero)
+print(type(numero))
