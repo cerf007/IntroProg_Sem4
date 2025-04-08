d47ce3e1848a8e4333adf992b9cea065e35fd47d
cerf007 <cerf02062007@gmail.com>
2025-04-08 10:38:31 -0600

    Ejercicios de la sesión 2

---
+++ Sesion2/ejercicio1.py
@@ -0,0 +1,8 @@
+#Escribe un algoritmo que declare tres variables: una para el nombre de una persona, otra para su edad y otra para su estatura. 
+# Asigna valores adecuados a cada una.
+nombre_completo = input("Por favor, ingrese su nombre completo: ")
+edad =  input("Por favor, ingrese su edad: ")
+altura =  input("Por último, ingrese su altura: ")
+print("Nombre completo: ",nombre_completo)
+print("Edad:", edad)
+print("Altura: ", altura)
---
+++ Sesion2/ejercicio2.py
@@ -0,0 +1,2 @@
+#Declara tres variables numéricas y evalúa la siguiente expresión: (a + b) * c / 2.
+#Muestra el resultado y explica cómo se evalúa la expresión paso a paso
