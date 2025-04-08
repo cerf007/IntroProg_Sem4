e4d008ebe4ec9e251d631f6bacfbca0fa530f03f
cerf007 <cerf02062007@gmail.com>
2025-04-08 08:43:02 -0600

    Ejercicios de la semana 4, sesión 1

---
+++ ejercicio1.py
@@ -0,0 +1,17 @@
+#declarar variables
+nombre_completo = "Carlos Edaurdo Rodríguez Flores"
+edad = 17
+altura = 1.65
+soltero = True
+
+print(f"Nombre completo:{nombre_completo}")
+print(f"Tipo de dato:{type(nombre_completo)}")
+
+print(f"Edad: {edad}")
+print(f"Tipo de dato: {type(edad)}")
+
+print(f"Altura: {altura}")
+print(f"Tipo de dato: {type(edad)}")
+
+print(f"Estado civil: {soltero}")
+print(f"Tipo de dato: {type(soltero)}")
---
+++ ejercicio4.py
@@ -0,0 +1,6 @@
+print(5 + 3)#suma
+print(5 - 3)#resta
+print(5 * 3)#multiplicación
+print(5 / 3)#división
+print(5 % 3)#modulo
+print(5//3)#división entera
