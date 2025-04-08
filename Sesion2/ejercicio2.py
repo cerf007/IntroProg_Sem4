08b99e468782b7f7559029e8bcceff63bdbe7199
cerf007 <cerf02062007@gmail.com>
2025-04-08 11:19:22 -0600

    Ejercicio de la sesión 2

--- Sesion2/ejercicio2.py
+++ Sesion2/ejercicio2.py
@@ -4,7 +4,7 @@ print("Bienvenido a tu calculadora de la expresión (a + b) * c / 2")
 variable_a = float(input("Ingrese el valor númerico a: "))
 variable_b = float(input("Ingrese el valor númerico b: "))
 variable_c = float(input("Ingrese el valor númerico c: "))
-operacion = float((variable_a + variable_b) * variable_c) / 2
+operacion = float(variable_a + variable_b) * variable_c / 2
 print("Primero se realiza la suma de varibale a", variable_a, "con la variable b", variable_b)
 print("Luego, del resultado del parenteris se multiplica con la varible c", variable_c)
 print("Por último, se divide el todo eel resulatado entre 2 para obtener", operacion)
---
+++ Sesion2/ejercicio3.py
@@ -0,0 +1,5 @@
+#Escribe un algoritmo que reciba el nombre y apellido de una persona, y luego
+#muestre el nombre completo concatenado.
+nombre = input("Por fvor ingrese solamente sus 2 nombres: ")
+apellidos = input("Por favor ingrese a continuación sus 2 apellidos: ")
+print(f"Su nombre completo es: {nombre}  {apellidos}")
