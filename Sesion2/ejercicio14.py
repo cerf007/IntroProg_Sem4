b287cfac64bb49e82c087afe88719a4afc480b55
cerf007 <cerf02062007@gmail.com>
2025-04-10 10:22:18 -0600

    Ejercicio del correo

---
+++ Sesion2/ejercicio14.py
@@ -0,0 +1,6 @@
+#Pide nombre y apellido y muestra un posible correo:
+#nombre.apellido@miuniversidad.edu.ni
+nombre = input("Por favor, ingrese solamente su primer nombre: ")
+apelllido = input("Ahora, ingrese solamente su primer apellido: ") 
+print(f"Su psosible correo sería: {nombre}.{apelllido}@uam.edu.ni")
+
