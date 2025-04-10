146a43da3977b01b577fdc5429d0846d8899ab04
cerf007 <cerf02062007@gmail.com>
2025-04-09 18:07:50 -0600

    Ejercicio del interés a pagar

---
+++ Sesion2/ejercicio8.py
@@ -0,0 +1,8 @@
+#Una persona recibe un préstamo de 10,000.00 de un banco 
+#se desea saber cuanto pagará de interés, si el banco le cobra una tasa del 27% anual.
+
+prestamo = 10000
+tasa_anual = 0.24
+interes = prestamo * tasa_anual
+
+print(f"El interés a pagar del prestamo de {prestamo} con un tasa anual del 24% es de {interes:.2f}")
